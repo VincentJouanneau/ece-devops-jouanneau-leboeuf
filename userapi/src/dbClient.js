dde6ed30e37984e5ee6e12e66c1bc1a6244d7f90
LeboeufAdrien <78808956+AdrienLeboeuf@users.noreply.github.com>
2022-12-23 14:42:55 +0100

    fix: update

--- userapi/src/dbClient.js
+++ userapi/src/dbClient.js
@@ -3,8 +3,8 @@ const configure = require('./configure')
 
 const config = configure()
 var db = redis.createClient({
-  host: config.env.redis.host,
-  port: config.env.redis.port,
+  host: process.env.redis.host,
+  port: process.env.redis.port,
   retry_strategy: () => {
     return new Error("Retry time exhausted")
   }
