14e67a7d0893baf3dec6727cd9297e277d25c6eb
LeboeufAdrien <78808956+AdrienLeboeuf@users.noreply.github.com>
2022-12-23 15:02:02 +0100

    JEGKRTLKGR,

--- userapi/src/dbClient.js
+++ userapi/src/dbClient.js
@@ -1,31 +1,17 @@
-const redis = require("redis");
+var redis = require("redis");
+const configure = require('./configure')
 
-// Creates a new Redis client
-// If REDIS_HOST is not set, the default host is localhost
-// If REDIS_PORT is not set, the default port is 6379
-const redisClient = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT  
+const config = configure()
+var db = redis.createClient({
+  host: config.redis.host,
+  port: config.redis.port,
+  retry_strategy: () => {
+    return new Error("Retry time exhausted")
+  }
+})
+
+process.on('SIGINT', function() {
+  db.quit();
 });
 
-redisClient.on("error", function(err) {
-    console.log("Error " + err);
-});
-
-// Sets the key "octocat" to a value of "Mona the octocat"
-redisClient.set("octocat", "Mona the Octocat", redis.print);
-// Sets a key to "octocat", field to "species", and "value" to "Cat and Octopus"
-redisClient.hset("species", "octocat", "Cat and Octopus", redis.print);
-// Sets a key to "octocat", field to "species", and "value" to "Dinosaur and Octopus"
-redisClient.hset("species", "dinotocat", "Dinosaur and Octopus", redis.print);
-// Sets a key to "octocat", field to "species", and "value" to "Cat and Robot"
-redisClient.hset(["species", "robotocat", "Cat and Robot"], redis.print);
-// Gets all fields in "species" key
-
-redisClient.hkeys("species", function (err, replies) {
-    console.log(replies.length + " replies:");
-    replies.forEach(function (reply, i) {
-        console.log("    " + i + ": " + reply);
-    });
-    redisClient.quit();
-});
+module.exports = db
